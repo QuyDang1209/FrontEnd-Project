de41f7687d9e61c9035296f3256e3ec6749ce2e2
NguyenVanAnh98 <nguyenanhtk321@gmail.com>
2024-07-03 10:36:57 +0700

    FE-BE edit Profile

--- houseforrent/src/App.js
+++ houseforrent/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ProfileForm from "./Component/ProfileForm/ProfileForm";
+import {Avatar, Button, TextField} from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+
+            <header className="App-header">
+                <ProfileForm />
+            </header>
+        </div>
+    );
+};
 
 export default App;
