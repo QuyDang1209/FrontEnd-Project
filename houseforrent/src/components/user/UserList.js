6d8219df1e2ce3b2cf74b617afaab57c3f7017a1
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-07-05 09:35:58 +0700

    Quy mad , fix view condition and done

--- houseforrent/src/components/user/UserList.js
+++ houseforrent/src/components/user/UserList.js
@@ -9,11 +9,13 @@ import {
     TableRow,
     Paper,
     Typography,
-    FormControl,
-    Select,
-    MenuItem,
     Box,
     Switch,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl,
+    TextField,
     Button,
     Dialog,
     DialogTitle,
@@ -26,41 +28,7 @@ const UserList = () => {
     const [users, setUsers] = useState([]);
     const [error, setError] = useState(null);
     const [selectedUser, setSelectedUser] = useState(null);
-    const [roleId, setRoleId] = useState("");
-    const handleChange = async (id, newRole) =>{
-        console.log("id " + id+ " role " + newRole);
-        
-        console.log(roleId);
-        try{
-            await axios.patch(`http://localhost:8080/api/user/change-role`, [{
-                id: id,
-                role: newRole
-            }], {
-                headers: {
-                    'Authorization': 'Bearer <your_access_token>',
-                    'Content-Type': 'application/json'
-                }
-            });
-            const updatedUsers = users.map(user => {
-                if (user.id === id) {
-                    return {
-                        ...user,
-                        role: [{
-                            rolename: newRole == "1" ? "user" : "host",
-                            id: newRole == "1" ? 1 : 2
-                        }]
-                    };
-                }
-                return user;
-                
-            });
-            console.log(updatedUsers);
-            setUsers(updatedUsers);
-        }
-        catch(error){
-            setError('Lỗi khi thay đổi vai trò: '+ error.message);
-        }
-    }
+
     useEffect(() => {
         fetchUsers();
     }, []);
@@ -99,7 +67,7 @@ const UserList = () => {
                     return {
                         ...user,
                         active: {
-                            active: newStatus ? 'OPEN' : 'CLOSE'
+                            active: newStatus ? 'open' : 'close'
                         }
                     };
                 }
@@ -114,6 +82,42 @@ const UserList = () => {
     const handleSwitchChange = (userId, isChecked) => {
         handleChangeStatus(userId, isChecked);
     };
+    const [checked, setChecked] = useState(true);
+    const [unchecked, setUnChecked] = useState(false);
+    const [roleId, setRoleId] = useState("");
+    const handleChange = async (id, newRole) =>{
+        console.log("id " + id+ " role " + newRole);
+        console.log(roleId);
+        try{
+            await axios.patch(`http://localhost:8080/api/user/change-role`, [{
+                id: id,
+                role: newRole
+            }], {
+                headers: {
+                    'Authorization': 'Bearer <your_access_token>',
+                    'Content-Type': 'application/json'
+                }
+            });
+            const updatedUsers = users.map(user => {
+                if (user.id === id) {
+                    return {
+                        ...user,
+                        role: [{
+                            rolename: newRole === "1" ? "User" : "Host",
+                            id: newRole === "1" ? 1 : 2
+                        }]
+                    };
+                }
+                return user;
+                
+            });
+            console.log(updatedUsers);
+            setUsers(updatedUsers);
+        }
+        catch(error){
+            setError('Lỗi khi thay đổi vai trò: '+ error.message);
+        }
+    }
 
     const handleViewClick = (user) => {
         setSelectedUser(user);
@@ -124,7 +128,7 @@ const UserList = () => {
     };
 
     return (
-        <Box sx={{ maxWidth: 800, mx: 'auto', mt: 4 }}>
+        <Box sx={{ maxWidth: 1000, mx: 'auto', mt: 4 }} >
             <Paper elevation={3} sx={{ p: 4 }}>
                 <Typography variant="h4" component="h2" gutterBottom>
                     Danh sách người dùng
@@ -149,15 +153,17 @@ const UserList = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {users.map((user) => (
-                                <TableRow key={user.id}>
-                                    <TableCell>{user.name}</TableCell>
-                                    <TableCell>{user.address}</TableCell>
-                                    <TableCell>{user.phone}</TableCell>
-                                    <TableCell>{user.dob}</TableCell>
-                                    <TableCell>{user.email}</TableCell>
-                                    <TableCell>
-                                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            {users.map((user) => {
+                                if(user.role[0].id !==3){
+                                    return ((
+                                        <TableRow key={user.id}>
+                                            <TableCell>{user.name}</TableCell>
+                                            <TableCell>{user.address}</TableCell>
+                                            <TableCell>{user.phone}</TableCell>
+                                            <TableCell>{new Date(user.dob).toLocaleDateString('en-GB')}</TableCell>
+                                            <TableCell>{user.email}</TableCell>
+                                            <TableCell>
+                                            <FormControl sx={{ m: 1, minWidth: 120 }}>
                                                     <Select
                                                     id="select"
                                                     value={user.role[0].id}
@@ -167,30 +173,32 @@ const UserList = () => {
                                                     <MenuItem value={2} > Host</MenuItem>
                                                     </Select>
                                             </FormControl>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Switch
-                                            checked={user.active.active === 'OPEN'}
-                                            onChange={(event) => handleSwitchChange(user.id, event.target.checked)}
-                                            inputProps={{ 'aria-label': 'controlled' }}
-                                        />
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button
-                                            variant="contained"
-                                            color="primary"
-                                            onClick={() => handleViewClick(user)}
-                                        >
-                                            View
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Switch
+                                                    checked={user.active.active === 'open' ? checked : unchecked }
+                                                    onChange={(event) => handleSwitchChange(user.id, event.target.checked)}
+                                                    inputProps={{ 'aria-label': 'controlled' }}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                <Button
+                                                    variant="contained"
+                                                    color="primary"
+                                                    onClick={() => handleViewClick(user)}
+                                                >
+                                                    View
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            })}
                         </TableBody>
                     </Table>
                 </TableContainer>
                 <Dialog
-                    open={!!selectedUser}
+                    open={selectedUser}
                     onClose={handleCloseDetail}
                     maxWidth="md"
                     fullWidth
